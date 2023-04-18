54622a86ec0380ba6d214f8c0db6621a9a4d9dad
sophe4rum <sohearum14@gmail.com>
2023-04-18 22:24:28 +0700

    react-hw002

---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Product() {
+  return (
+    <div className='card'>
+      <img/>
+      <h1>Title</h1>
+      <p>price</p>
+      <p>Description</p>
+    </div>
+  )
+}
